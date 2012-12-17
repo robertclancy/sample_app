2da9c04d6ef544ed0f859c7cd30cea3461133f95
Robert Clancy <rob.clancy@gmail.com>
2012-12-17 20:45:25 +0000

    Add sign out

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -24,6 +24,7 @@ describe "AuthenticationPages" do
         before { click_link "Home" }
         it { should_not have_selector('div.alert.alert-error') }
       end
+
     end
 
     context "with valid information" do
@@ -38,6 +39,11 @@ describe "AuthenticationPages" do
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
+
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
     end
   end
 end
